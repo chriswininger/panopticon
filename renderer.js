d0221f307760d32d540409786bd0f0fc2a48d3a1
chriswininger <chris.wininger@gmail.com>
2017-02-26 11:36:03 -0500

    Initial Commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,36 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const cv = require('opencv');
+
+cv.readImage('./MeAndMcAffee.jpeg', function(err, img) {
+	if (err) {
+		console.error('error: ' + error);
+	}
+
+/*	const width = img.width();
+	const height = img.height();
+
+	if (width < 1 || height < 1) {
+		conole.error('image has no size?!');
+	}
+
+	img.convertGrayscale();
+   cv.imgShow('img', img);
+
+	const face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml');
+	const eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml');
+	const faces = face_cascade.detectMultiScale(img, 1.3, 5);
+
+*/
+	img.convertGrayscale();
+	img.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
+		if (err)
+			return console.log('Error: ' + err);
+
+		console.log('found %s face(s)', faces.length);
+		for (let i = 0; i < faces.length; i++) {
+			let x = faces[i];
+			img.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+		}
+
+		img.save('./newImage_' + Date.now() + '.jpg');
+	});
+});
